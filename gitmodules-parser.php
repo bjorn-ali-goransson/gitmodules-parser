e5b4cab45224e56bc1e07eaae3ce99bfaa2a1e81
Björn Ali Göransson <bornemix@gmail.com>
2013-08-26 19:30:28 +0200

    rename path to local_path

--- gitmodules-parser.php
+++ gitmodules-parser.php
@@ -18,10 +18,10 @@ function gitmodules_get_all($dir = '.'){
       $submodule->parent_gitmodules_path = $gitmodules_path;
 
       $submodule->name = $submodule_name;
-      $submodule->path = gitmodules_get_path($contents[++$i]);
+      $submodule->local_path = gitmodules_get_path($contents[++$i]);
       $submodule->url = gitmodules_get_url($contents[++$i]);
       
-      $submodule->dir_exists = file_exists(dirname($submodule->parent_gitmodules_path) . '/' . $submodule->path);
+      $submodule->dir_exists = file_exists(dirname($submodule->parent_gitmodules_path) . '/' . $submodule->local_path);
       
       $submodule->is_github = strpos($submodule->url, '://github.com') !== FALSE;
       
