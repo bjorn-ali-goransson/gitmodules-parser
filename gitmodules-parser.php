2c6c5139a19bb3317854d56fa3a66f9f47a04cdc
Björn Ali Göransson <bornemix@gmail.com>
2013-08-26 19:31:03 +0200

    rename dir_exists to path_exists

--- gitmodules-parser.php
+++ gitmodules-parser.php
@@ -21,7 +21,7 @@ function gitmodules_get_all($dir = '.'){
       $submodule->local_path = gitmodules_get_path($contents[++$i]);
       $submodule->url = gitmodules_get_url($contents[++$i]);
       
-      $submodule->dir_exists = file_exists(dirname($submodule->parent_gitmodules_path) . '/' . $submodule->local_path);
+      $submodule->path_exists = file_exists(dirname($submodule->parent_gitmodules_path) . '/' . $submodule->local_path);
       
       $submodule->is_github = strpos($submodule->url, '://github.com') !== FALSE;
       
